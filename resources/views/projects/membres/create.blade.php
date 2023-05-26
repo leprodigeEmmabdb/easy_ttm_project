4e413c23d135072179046113b03519c582d2ccf4
emmanuelbdb <emmanuelbadibanga250gmail.com>
2023-05-26 14:45:12 +0100

    gestion de listage des contributeurs par projet

--- resources/views/projects/membres/create.blade.php
+++ resources/views/projects/membres/create.blade.php
@@ -1,110 +1,106 @@
-@extends('layouts.app')
 
-@section('content')
-    <section class="card">
-        <div class="">
-            <div class="card-header">
-                <h3 class="card-title">Projects Detail</h3>
+   <section class="content">
+    <div class="col-md-12 px-5">
+        <div class="card card-primary collapsed-card">
+          <div class="card-header">
+            <h3 class="card-title">Formulaire</h3>
 
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                        <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                        <i class="fas fa-times"></i>
-                    </button>
-                </div>
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                <i class="fas fa-plus"></i>
+              </button>
             </div>
-            <div class="card-body">
-                <div class="card card-orange col-lg-8 mx-auto my-4 ">
-                    <form class="form-horizontal " action="{{ route('membres.store', $project->id) }}" method="POST">
-                        @csrf
-                        <div class="card-body row">
-                            <div class="col-lg-4">
-                                <div class="form-group row">
-                                    <label for="inputEmail3" class="col-sm-2 col-form-label">Nom</label>
-                                    <select class="form-control select2 col-sm-9" style="width: 100%;"
-                                        aria-placeholder="type de projet" name="user">
-                                        @forelse ($users as $item)
-                                            <option value="{{ $item->id }}">{{ $item->name }}</option>
-                                        @empty
-                                            <option>Aucune suggestion</option>
-                                        @endforelse
+          </div>
+          <div class="card-body" style="display: none;">
+                <form class="form-horizontal " action="{{ route('membres.store', $project->id) }}" method="POST">
+                    @csrf
+                    <div class="card-body row">
+                        <div class="col-lg-4">
+                            <div class="form-group row">
+                                <label for="inputEmail3" class="col-sm-2 col-form-label">Nom</label>
+                                <select class="form-control select2 col-sm-9" style="width: 100%;"
+                                    aria-placeholder="type de projet" name="user">
+                                    @forelse ($users as $item)
+                                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                    @empty
+                                        <option>Aucune suggestion</option>
+                                    @endforelse
 
-                                    </select>
-                                </div>
+                                </select>
+                            </div>
+                            <div class="col-sm-10">
+                                <input type="hidden" class="form-control" id="inputEmail3" value="{{ $project->id }}"
+                                    placeholder="email" name="project">
+                            </div>
+                            <div class="form-group row">
+                                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
                                 <div class="col-sm-10">
-                                    <input type="hidden" class="form-control" id="inputEmail3" value="{{ $project->id }}"
-                                        placeholder="email" name="project">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputEmail3" placeholder="email"
-                                            name="email">
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <label for="inputEmail3" class="col-sm-2 col-form-label">Tél.</label>
-                                    <div class="col-sm-10">
-                                        <input type="tel" class="form-control" id="inputEmail3" placeholder="tel..."
-                                            name="phone">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="inputEmail3" class="col-sm-2 col-form-label">Rôle</label>
-                                    <select class="form-control select2 col-sm-9" style="width: 100%;"
-                                        aria-placeholder="type de projet" name="role">
-                                        <option value="Contributeurs">Contributeurs</option>
-                                        <option value="Steam">Steam</option>
-                                        <option value="Validateur">Validateur</option>
-                                    </select>
+                                    <input type="email" class="form-control" id="inputEmail3" placeholder="email"
+                                        name="email">
                                 </div>
                             </div>
-                            <div class="offset-1 col-lg-7">
 
-                                <div class="form-group row">
-                                    <label for="inputEmail3" class="col-sm-2 col-form-label">Direction</label>
-                                    <div class="col-sm-10">
-                                        <input type="tel" class="form-control" id="inputEmail3"
-                                            placeholder="direction...">
-                                    </div>
+                            <div class="form-group row">
+                                <label for="inputEmail3" class="col-sm-2 col-form-label">Tél.</label>
+                                <div class="col-sm-10">
+                                    <input type="tel" class="form-control" id="inputEmail3" placeholder="tel..."
+                                        name="phone">
                                 </div>
-                                <div class="form-group row">
-                                    <label for="inputEmail3" class="col-sm-2 col-form-label">Manager</label>
-                                    <div class="col-sm-10">
-                                        <input type="tel" class="form-control" id="inputEmail3"
-                                            placeholder="manager...">
-                                    </div>
-                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="inputEmail3" class="col-sm-2 col-form-label">Rôle</label>
+                                <select class="form-control select2 col-sm-9" style="width: 100%;"
+                                    aria-placeholder="type de projet" name="role">
+                                    <option value="Contributeurs">Contributeurs</option>
+                                    <option value="Steam">Steam</option>
+                                    <option value="Validateur">Validateur</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="offset-1 col-lg-7">
 
-                                <div class="form-group row">
-                                    <label for="inputPassword3" class="col-sm-2 col-form-label">Métier</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="inputPassword3" placeholder="métier">
-                                    </div>
+                            <div class="form-group row">
+                                <label for="inputEmail3" class="col-sm-2 col-form-label">Direction</label>
+                                <div class="col-sm-10">
+                                    <input type="tel" class="form-control" id="inputEmail3"
+                                        placeholder="direction...">
                                 </div>
-                                <div class="form-group row">
-                                    <div class="offset-sm-2 col-sm-10">
-                                        <div class="form-check">
-                                            <input type="checkbox" class="form-check-input" id="exampleCheck2">
-                                            <label class="form-check-label" for="exampleCheck2">le notifier</label>
-                                        </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="inputEmail3" class="col-sm-2 col-form-label">Manager</label>
+                                <div class="col-sm-10">
+                                    <input type="tel" class="form-control" id="inputEmail3"
+                                        placeholder="manager...">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="inputPassword3" class="col-sm-2 col-form-label">Métier</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputPassword3" placeholder="métier">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="offset-sm-2 col-sm-10">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck2">
+                                        <label class="form-check-label" for="exampleCheck2">le notifier</label>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <!-- /.card-body -->
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">Ajouter</button>
-                            <button type="reset" class="btn btn-default float-right">Cancel</button>
-                        </div>
-                        <!-- /.card-footer -->
-                    </form>
-                </div>
-            </div>
-            <!-- /.card-body -->
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Ajouter</button>
+                        <button type="reset" class="btn btn-default float-right">Cancel</button>
+                    </div>
+                    <!-- /.card-footer -->
+                </form>
+          </div>
+          <!-- /.card-body -->
         </div>
-    </section>
-@endsection
+        <!-- /.card -->
+      </div>
+   </section>
+
--- resources/views/projects/membres/index.blade.php
+++ resources/views/projects/membres/index.blade.php
@@ -1,76 +1,82 @@
 @extends('layouts.app')
+@section('title')
+    <a href="#">Membre</a>
+@endsection
 @section('content')
     <div class="content">
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Membres</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Projets</li>
-                        </ol>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
+        @include('projects.membres.create')
         <section class="content">
-            <div class="text-right mx-4">
-                <a class="btn btn-primary m-2 " href="{{ route('membres.create', $project->id) }}"><b>+</b></a>
-            </div>
-            <div class="card">
+            <div class="col-md-12 px-5">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Liste</h3>
 
-                <div class="card-body p-0">
-                    <table class="table table-striped projects">
-                        <thead>
-                            <tr>
-                                <th>N°</th>
-                                <th>Nom & Prénom</th>
-                                <th class="text-center">Contribution</th>
-                                <th class="text-center">Rôle</th>
-                                <th class="text-center">Statut</th>
-                                <th class="text-center">Action</th>
-
-                            </tr>
-                            @forelse ($members as $item)
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped projects">
+                            <thead>
                                 <tr>
-                                    <td>{{ $item->id }}</td>
-                                    <td>
-                                        <a href="#">{{ $item->user_id }}</a><br>
-                                        <small>
-                                            <b class="text-black-50">Masculin</b>
-                                        </small>
-                                    </td>
+                                    <th>N°</th>
+                                    <th>Nom & Prénom</th>
+                                    <th class="text-center">Contribution</th>
+                                    <th class="text-center">Rôle</th>
+                                    <th class="text-center">Statut</th>
+                                    <th class="text-center">Action</th>
 
-                                    <td class="project_progress ">
-                                        <div class="progress progress-sm">
-                                            <div class="progress-bar bg-red" role="progressbar" aria-valuenow="57"
-                                                aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
-                                        </div>
-                                        <small>loading...</small>
-                                    </td>
-                                    <td class="project-state ">
-                                        {{ $item->role }}
-                                    </td>
-                                    <td class="project-state">
-                                        <span class="badge badge-success p-2">{{ $item->status }}</span>
-                                    </td>
-                                    <td class="project-actions text-center">
-                                        <a class="btn btn-primary btn-sm" href="#"><i class="fas fa-folder"></i></a>
-                                        <a class="btn btn-info btn-sm" href="#"><i class="fas fa-pencil-alt"></i></a>
-                                        <a class="btn btn-danger btn-sm" href="#"><i class="fas fa-trash"></i></a>
-                                    </td>
                                 </tr>
-                            @empty
-                                <tr class="col-lg-12  text-center text-black-50 h5">
-                                    <td colspan="6">Aucun membre</td>
-                                </tr>
-                            @endforelse
-                        </thead>
-                    </table>
+                                @forelse ($members as $item)
+                                    <tr>
+                                        <td>{{ $item->id }}</td>
+                                        <td>
+                                            {{ $item->user_id }}<br>
+                                            <small>
+                                                <b class="text-black-50">Masculin</b>
+                                            </small>
+                                        </td>
+
+                                        <td class="project_progress ">
+                                            <div class="progress progress-sm">
+                                                <div class="progress-bar bg-red" role="progressbar" aria-valuenow="57"
+                                                    aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
+                                            </div>
+                                            <small>loading...</small>
+                                        </td>
+                                        <td class="project-state ">
+                                            {{ $item->role }}
+                                        </td>
+                                        <td class="project-state">
+                                            <span class="badge badge-success p-2">{{ $item->status }}</span>
+                                        </td>
+                                        <td class="project-actions text-center">
+                                            <a class="btn btn-primary btn-sm" href="#"><i
+                                                    class="fas fa-folder"></i></a>
+                                            <a class="btn btn-info btn-sm" href="#"><i
+                                                    class="fas fa-pencil-alt"></i></a>
+                                            <a class="btn btn-danger btn-sm" href="#"><i class="fas fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr class="col-lg-12  text-center text-black-50 h5">
+                                        <td colspan="6">Aucun membre</td>
+                                    </tr>
+                                @endforelse
+                            </thead>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer clearfix">
+                        <ul class="pagination pagination-xl m-0 float-right">
+                            <li class="page-item m-2"><a class="page-link" href="{{ $members->previousPageUrl() }}">«</a></li>
+                            <li class="page-item m-2"><a class="page-link" href="{{ $members->nextPageUrl() }}">»</a></li>
+                        </ul>
+                    </div>
                 </div>
+                <!-- /.card -->
             </div>
         </section>
     </div>
