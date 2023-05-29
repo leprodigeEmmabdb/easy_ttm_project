3cf9c8e1b299adfe6c2cb4d672125277503e8287
emmanuel badibanga <emmanuelbadibanga250@gmail.com>
2023-05-29 02:11:19 +0100

    Demande:création du modèle, migration et autres organes de gestion de données

---
+++ database/factories/DemandeFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Demande>
+ */
+class DemandeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'model' => 'model.pdf',
+            'description' => fake()->paragraph(),
+            'contributeur' => fake()->name(),
+            'delais' => fake()->numberBetween(0,10),
+            'status' => 'non soumis',
+        ];
+    }
+}
