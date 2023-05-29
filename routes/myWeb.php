c37ad720a8af4b9f878137f93729044dd8345af3
emmanuel badibanga <emmanuelbadibanga250@gmail.com>
2023-05-29 02:23:58 +0100

    Route: création de la route pour des demandes

--- routes/myWeb.php
+++ routes/myWeb.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('projects', App\Http\Controllers\ProjectController::class);
 Route::resource('projects/{id}/membres', App\Http\Controllers\ProjectUserController::class);
+Route::resource('demande', App\Http\Controllers\DemandeController::class);
